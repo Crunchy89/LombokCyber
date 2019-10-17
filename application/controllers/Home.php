c73c38e9e6a29d410bb73c9558ed995606ec73f4
Chrunchy89 <rocker.hunt@gmail.com>
2019-10-17 16:40:24 +0800

    new

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,8 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
-
-	
 	public function index()
 	{
 		$data['title']='LC Reg Loteng';
--- application/views/templates/home_footer.php
+++ application/views/templates/home_footer.php
@@ -1,4 +1,45 @@
 
+		
+
+		<!-- Modal login -->
+		<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+			aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="exampleModalLabel">Login</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<form action="" method="post">
+							<div class="form-group">
+								<label for="username">Username</label>
+								<input type="text" class="form-control" name="username" id="username" placeholder="Username / Email">
+							</div>
+							<div class="form-group">
+								<label for="password">Password</label>
+								<div class="input-group">
+									<input type="password" name="password" id="password" class="form-control" placeholder="Password"
+										aria-label="Password" aria-describedby="basic-addon2">
+									<div class="input-group-append">
+										<span onclick="see()" class="input-group-text" id="basic-addon2"><i id="slash"
+												class="fas fa-fw fa-eye"></i></span>
+									</div>
+								</div>
+							</div>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+						<button type="submit" class="btn btn-primary">Login</button>
+					</div>
+					</form>
+				</div>
+			</div>
+		</div>
+
+
 		<script src="<?=base_url('assets/vendor/jquery/jquery.min.js')?>"></script>
 		<script src="<?=base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
 		<script src="<?=base_url('assets/js/style.js')?>"></script>
