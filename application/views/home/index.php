d3a5e6c129f23386a39d5f3e0a6af805e0f6ee81
Chrunchy89 <rocker.hunt@gmail.com>
2019-10-17 16:58:48 +0800

    new

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -4,9 +4,20 @@
     </div>
     <div class="berita mt-1 mb-1 border d-flex align-content-center bg-light">
 	<div class="container-fluid">
-    <h6>Berita Terkini</h6>
+	<h6 class="mt-2">Berita Terkini</h6>
+	<div class="col-md-8 mt-3 mb-3">
+    <div class="container-fluid bg-light row">
+	<div class="row border mt-2 mb-5 d-flex ">
+    <h4 class="text-center mt-2 mb-1">
+	Pendaftaran Anggota Baru LC Reg. Loteng
+	</h4>
+	Mari bergabung bersama kami
+	<div>
+	Upgrade Your Skills with Us <a href="">Join</a>
+	</div>
+	</div>
 	</div>
     </div>
-    <div class="container-fluid border bg-light">
-    tes
+    
+	</div>
     </div>
